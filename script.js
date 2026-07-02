b16aa1a2f1ea8a86fabd3d7bf90b52bef9f55c55
Jeevith BS <jeevithbs.gowda12@gmail.com>
2026-07-02 17:52:13 +0530

    Added square, square root, reciprocal and backspace

--- script.js
+++ script.js
@@ -14,4 +14,40 @@ function calculate() {
     } catch {
         display.value = "Error";
     }
+
+// Square Root (√)
+function squareRoot() {
+    if (display.value === "") {
+        return;
+    }
+
+    display.value = Math.sqrt(Number(display.value));
+}
+
+// Square (x²)
+function square() {
+    if (display.value === "") {
+        return;
+    }
+
+    display.value = Math.pow(Number(display.value), 2);
+}
+
+// Reciprocal (1/x)
+function reciprocal() {
+
+    if (display.value === "" || Number(display.value) === 0) {
+        display.value = "Error";
+        return;
+    }
+
+    display.value = 1 / Number(display.value);
+}
+
+// Backspace (⌫)
+function backspace() {
+
+    display.value = display.value.slice(0, -1);
+
+}
 }
